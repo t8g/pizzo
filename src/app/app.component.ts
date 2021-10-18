bfb9dbf5dea5f0d7873a2b0996c45f9f076aad68
Sandy Goldscheider <sgoldscheider@lucca.fr>
2021-10-18 15:07:21 +0200

    [add] json in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import * as pizzaList from './pizzas.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pizzo';
+  public pizzas = (pizzaList as any).default;
 }
